29403f2654945b62d3294e06b1608c5d3b7264f2
H2Sxxa <h2sxxa0w0@gmail.com>
2023-10-01 19:57:48 +0800

    📝 Doc of type

--- lib/data/typed.dart
+++ lib/data/typed.dart
@@ -1,25 +1,57 @@
 class Eatery {
   String location;
   String name;
+  String description;
   List<String>? comments;
   List<Dish>? dishes;
+  List<String>? images;
+
+  ///```json
+  /// {
+  ///   "name":"eatry_0",
+  ///   "location":"where",
+  ///   "descrpiton":"",
+  ///   "comments":[],
+  ///   "dishes":[],
+  ///   "images":[]
+  /// }
+  ///
+  ///
+  ///
+  ///```
   Eatery(
       {this.location = "Unknown",
       this.name = "Unknown",
+      this.description = "Unknown",
       this.comments,
       this.dishes}) {
     comments ??= [];
     dishes ??= [];
+    images ??= [];
   }
 }
 
 class Dish {
   String name;
   double price;
+  String description;
   List<String>? comments;
-  Dish({this.name = "Unknown", this.price = -1, this.comments}) {
+  List<String>? images;
+
+  ///```json
+  /// {
+  ///   "name": "dish_0",
+  ///   "price": "14.0",
+  ///   "comments": [],
+  ///   "images":[]
+  /// }
+  /// ```
+  Dish(
+      {this.name = "Unknown",
+      this.description = "Unknown",
+      this.price = -1,
+      this.comments}) {
     comments ??= [];
+    images ??= [];
   }
 }
-
-
